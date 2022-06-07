5c6b0491a794d36ff4e347d1e8e606c7e42ec022
Andrew Gomez <drewgomez209@gmail.com>
2022-06-07 12:40:45 -0700

    posts-router working

--- api/server.js
+++ api/server.js
@@ -2,10 +2,13 @@
 // require your posts router and connect it here
 // ORDER IS IMPORTANT
 const express = require('express');
+const postsRouter = require('./posts/posts-router')
 
 const server = express();
 
 server.use(express.json()); // teach express how to parse json bodys
+
+server.use('.api/posts', postsRouter)
 
 server.use('*', (req, res) => {
     res.status(404).json({ message: 'not found' })
