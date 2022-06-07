fe2b0470519cc3cedfa0de00e8bc36ed304727f6
Andrew Gomez <drewgomez209@gmail.com>
2022-06-07 14:20:46 -0700

    wip

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,23 +1,70 @@
-// implement your posts router here
 const express = require('express')
+const Post = require('./posts-model')
 const router = express.Router()
 
-
-
+// GET THEM ALL
 router.get('/', (req, res) => {
-    res.json("yoooo")
+    Post.find()
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The posts information could not be retrieved" })
+        })
 })
 
+// GET BY SPECIFIC ID
 router.get('/:id', (req, res) => {
-
+    Post.findById(req.params.id)
+        .then(posts => {
+            if (posts) {
+                res.json(posts)
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" })
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The post information could not be retrieved" })
+        })
 })
 
+//
 router.post('/', (req, res) => {
-
+    let { title, contents } = req.body
+    if (!title || !contents) {
+        return res.status(400).json({ message: "Please provide title and contents for the post" })
+    } else {
+        title = title.trim();
+        contents = contents.trim();
+        Post.insert(req.body)
+            .then(({ id }) => {
+                return Post.findById(id)
+            })
+            .then(post => {
+                res.status(201).json(post)
+            })
+            .catch(error => {
+                res.status(500).json({
+                    message: "There was an error while saving the post to the database",
+                    err: err.message,
+                    stack: err.stack,
+                })
+            })
+    }
 })
 
 router.delete('/:id', (req, res) => {
-
+    Post.remove(req.params.id)
+        .then(post => {
+            if (!req.params.id) {
+                res.status(200).json({ message: 'The adopter has been nuked' });
+            } else {
+                res.status(404).json({ message: 'The adopter could not be found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The post could not be removed" })
+        })
 })
 
 router.put('/:id', (req, res) => {
