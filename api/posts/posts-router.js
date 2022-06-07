a7352fde8c7409d3994cb5fb8ebf271b1915cfed
Andrew Gomez <drewgomez209@gmail.com>
2022-06-07 12:37:53 -0700

    server listening

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,48 @@
 // implement your posts router here
+const Posts = require('.posts-router')
+const express = require('express')
+const router = express.Router()
+
+router.get('/', (req, res) => {
+    Posts.find(req.query)
+        .then(posts => {
+            res.status(200).json(posts)
+        })
+        .catch(error => {
+            res.status(500).json({ message: "The post with the specified ID does not exist" })
+        })
+})
+
+router.get('/:id', (req, res) => {
+    Posts.findById(req.params.id)
+        .then(post => {
+            if (post) {
+                res.status(200).json(post);
+            } else {
+                res.status(404).json({ message: "The post with the specified ID does not exist" });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: "The post information could not be retrieved" });
+        });
+});
+
+router.post('/:id/dogs', (req, res) => {
+    Posts.insert(req.params.id)
+        .then(post => {
+            if (post.length > 0) {
+                res.status(201).json(post);
+            } else {
+                res.status(404).json({ message: 'No dogs for this adopter' });
+            }
+        })
+        .catch(error => {
+            console.log(error);
+            res.status(500).json({ message: 'Error retrieving the dogs for this adopter' });
+        });
+});
+
+
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,15 @@
 // implement your server here
 // require your posts router and connect it here
+// ORDER IS IMPORTANT
+const express = require('express');
+
+const server = express();
+
+server.use(express.json()); // teach express how to parse json bodys
+
+server.use('*', (req, res) => {
+    res.status(404).json({ message: 'not found' })
+})
+
+
+module.exports = server;
